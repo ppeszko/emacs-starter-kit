246fdca73a5696ff7b127a859f2bf89d350c0a77
Patryk Peszko <patryk.peszko@xing.com>
2010-04-11 21:33:40 +0200

    new yasnippet

--- patryk.peszko.el
+++ patryk.peszko.el
@@ -4,5 +4,15 @@
 (global-set-key (kbd "A-/") 'comment-or-uncomment-region-or-line)
 
 (add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
-    (require 'textmate)
-    (textmate-mode)
+(require 'textmate)
+(textmate-mode)
+
+(add-to-list 'load-path
+                  "~/.emacs.d/vendor/yasnippet.el")
+(require 'yasnippet) ;; not yasnippet-bundle
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/vendor/yasnippet.el/snippets")
+
+(setq cursor-type "hollow")
+(setq default-cursor-in-non-selected-windows nil)
+(blink-cursor-mode 0.5)
