9dc4c42218c874350518fceeba52d78a668f9d06
Patryk Peszko <patryk.peszko@xing.com>
2010-04-11 18:47:47 +0200

    trying textmate support

--- patryk.peszko.el
+++ patryk.peszko.el
@@ -1,3 +1,8 @@
 ;;
 (menu-bar-mode 1)
 
+(global-set-key (kbd "A-/") 'comment-or-uncomment-region-or-line)
+
+(add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
+    (require 'textmate)
+    (textmate-mode)
