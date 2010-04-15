105f8f5ddd2cc0000d23ab88697d3950d351ea7c
Patryk Peszko <patryk.peszko@xing.com>
2010-04-15 20:40:46 +0200

    disabling peepopen

--- patryk.peszko.el
+++ patryk.peszko.el
@@ -48,11 +48,11 @@
 ;;(require 'color-theme)
 
 ;;OpenPeepcode
-(add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
-(require 'textmate)
-(textmate-mode)
-(add-to-list 'load-path "~/.emacs.d/vendor/")
-(require 'peepopen)
+;; (add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
+;; (require 'textmate)
+;; (textmate-mode)
+;; (add-to-list 'load-path "~/.emacs.d/vendor/")
+;; (require 'peepopen)
 
 ;; Color Themes
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/color-theme"))
