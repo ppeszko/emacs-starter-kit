6c24dfc0ebecaef5d67782bbdf8904cc867bac96
Patryk Peszko <patryk.peszko@xing.com>
2010-04-16 17:24:00 +0200

    enabling textmate mode

--- patryk.peszko.el
+++ patryk.peszko.el
@@ -4,9 +4,9 @@
 (global-set-key (kbd "A-/") 'comment-or-uncomment-region-or-line)
 
 ;;textmate-mode
-;;(add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
-;;(require 'textmate)
-;;(textmate-mode)
+(add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
+(require 'textmate)
+(textmate-mode)
 
 (add-to-list 'load-path
                   "~/.emacs.d/vendor/yasnippet.el")
