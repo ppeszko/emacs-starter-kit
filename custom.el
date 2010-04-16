f9110fa362b353dd53d48a7cac71888b8bea82e3
Patryk Peszko <patryk.peszko@xing.com>
2010-04-16 14:47:07 +0200

    default font is now monaco

--- custom.el
+++ custom.el
@@ -14,4 +14,5 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(html-mode-default ((t (:inherit sgml-mode-default :slant normal :weight normal :height 130 :family "monaco"))) t))
+ '(html-mode-default ((t (:inherit sgml-mode-default :slant normal :weight normal :height 130 :family "monaco"))) t)
+ '(text-mode-default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 130 :width normal :family "monaco"))) t))
